c1004b9798a28aa690c3efd0205ff8f1f6eb3542
Renan Vaz <renan.piresvz@gmail.com>
2021-05-17 18:46:19 -0300

    Fix formatting

--- auth0/connect.ts
+++ auth0/connect.ts
@@ -10,21 +10,23 @@ export function connectAppToAuth(
   /* The following function handles notifying Elm's app about possible failures and or success.
     It does not returns a feedback only when the user is redirected to the login page.
   */
-  const callback = (
-    lambda: (
-      client: Auth0Client,
-      autoLogin: boolean,
-    ) => Promise<auth.AuthSuccess | 'NO_FEEDBACK'>,
-  ) => async (): Promise<void> => {
-    try {
-      const result = await lambda(await authClient, authAutoLogin);
-      if (result !== 'NO_FEEDBACK') app.ports.authResult.send(result);
-    } catch (err: unknown) {
-      if (err instanceof auth.ElmTreatableError)
-        app.ports.authResult.send(err.toJSON());
-      else throw err;
-    }
-  };
+  const callback =
+    (
+      lambda: (
+        client: Auth0Client,
+        autoLogin: boolean,
+      ) => Promise<auth.AuthSuccess | 'NO_FEEDBACK'>,
+    ) =>
+    async (): Promise<void> => {
+      try {
+        const result = await lambda(await authClient, authAutoLogin);
+        if (result !== 'NO_FEEDBACK') app.ports.authResult.send(result);
+      } catch (err: unknown) {
+        if (err instanceof auth.ElmTreatableError)
+          app.ports.authResult.send(err.toJSON());
+        else throw err;
+      }
+    };
 
   app.ports.logout.subscribe(async () => auth.logout(await authClient));
   app.ports.login.subscribe(async () => auth.login(await authClient));
