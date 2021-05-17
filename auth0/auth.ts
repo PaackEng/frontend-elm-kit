7c24506e5b718a439425960d24c83fa194e8cc86
PedroHLC <root@pedrohlc.com>
2021-05-17 18:57:54 -0300

    Fix Auth's treatable errors

--- auth0/auth.ts
+++ auth0/auth.ts
@@ -14,7 +14,11 @@ class ElmTreatableError extends Error {
 
   constructor(tag: string, message: string) {
     super(message);
+    this.name = 'ElmTreatableError';
     this.tag = tag;
+
+    // https://github.com/microsoft/TypeScript/wiki/Breaking-Changes#extending-built-ins-like-error-array-and-map-may-no-longer-work
+    Object.setPrototypeOf(this, ElmTreatableError.prototype);
   }
 
   public toJSON(): AuthError {
