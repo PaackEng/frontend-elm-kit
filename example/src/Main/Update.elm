5c46b9cf0b28452e105c60d2348f36a9ef1c3d5f
Renan Vaz <renan.piresvz@gmail.com>
2021-05-28 11:34:03 -0300

    Modify the example to use Return

--- example/src/Main/Update.elm
+++ example/src/Main/Update.elm
@@ -5,27 +5,26 @@ import Main.Model exposing (Model, authConfig)
 import Main.Msg exposing (Msg(..))
 import Paack.Auth.Main as Auth
 import Paack.Effects as Effects exposing (Effects, fromLocal)
+import Paack.Return as R exposing (Return)
 
 
-update : Msg -> Model -> ( Model, Effects Msg )
+update : Msg -> Model -> Return Msg Model
 update msg model =
     case msg of
         ForAuth subMsg ->
             forAuth subMsg model
 
         LinkClicked _ ->
-            ( model, Effects.none )
+            R.singleton model
 
         RollbarFeedback _ ->
-            ( model, Effects.none )
+            R.singleton model
 
         UrlChanged newUrl ->
-            ( { model | url = newUrl }
-            , Effects.none
-            )
+            R.singleton { model | url = newUrl }
 
 
-forAuth : Auth.Msg -> Model -> ( Model, Effects Msg )
+forAuth : Auth.Msg -> Model -> Return Msg Model
 forAuth subMsg model =
     let
         ( subModel, effects ) =
@@ -33,10 +32,6 @@ forAuth subMsg model =
                 authConfig
                 subMsg
                 model.auth
-
-        mappedEffects =
-            fromLocal <| AuthEffect effects
     in
-    ( { model | auth = subModel, user = Auth.getUser subModel }
-    , mappedEffects
-    )
+    R.singleton { model | auth = subModel, user = Auth.getUser subModel }
+        |> R.withEffect (fromLocal <| AuthEffect effects)
