b43854bfcf5b955de40f9c369b1db26396dba9e9
PedroHLC <root@pedrohlc.com>
2021-08-24 14:26:07 -0300

    Init

---
+++ bin/build.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const fs = require('fs');
+const ElmPlugin = require('esbuild-plugin-elm');
+const EnvFilePlugin = require('esbuild-envfile-plugin');
+
+require('esbuild')
+  .build({
+    entryPoints: ['web/index.ts'],
+    outdir: 'dist/js',
+    entryNames: '[dir]/[name]-[hash]',
+    allowOverwrite: true,
+    bundle: true,
+    metafile: true,
+    plugins: [EnvFilePlugin, ElmPlugin()],
+  })
+  .then((result) => {
+    const htmlPath = 'dist/index.html';
+
+    fs.copyFile('web/index.html', htmlPath, console.log);
+
+    fs.readFile(htmlPath, 'utf8', (err, data) => {
+      if (err) return console.error(err);
+
+      const path = Object.keys(result.metafile.outputs)[0].replace('dist/', '');
+
+      fs.writeFile(
+        htmlPath,
+        data.replace('[OUTPUT_PATH]', path),
+        'utf8',
+        console.log,
+      );
+    });
+  });
