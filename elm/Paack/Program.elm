11339031230627a6c6a68f2295e694acf1ddb3f3
PedroHLC <root@pedrohlc.com>
2022-04-04 17:53:32 -0300

    Cleanup

--- elm/Paack/Program.elm
+++ elm/Paack/Program.elm
@@ -1,22 +1,12 @@
 module Paack.Program exposing (Program, browserApplication)
 
 import Browser exposing (UrlRequest)
-import Browser.Navigation as Nav exposing (Key)
-import Effects.LocalPerformer as LocalPerformer
-import Html exposing (Html, canvas, div)
-import Html.Attributes exposing (attribute, id)
-import Html.Lazy as Lazy exposing (lazy)
-import Main.Model as Model exposing (Flags, Model)
+import Main.Model exposing (Flags, Model)
 import Main.Msg exposing (Msg)
 import Main.Pages exposing (PageModel)
-import Main.Update exposing (update)
-import Paack.Effects as Effects
-import Paack.Effects.CommonPerformer as CommonPerformer
 import Paack.Effects.MainHelper exposing (PerformerModel, performedInit, performedUpdate)
-import Random
 import UI.Document as UI exposing (Document)
 import UI.RenderConfig exposing (RenderConfig)
-import UUID exposing (Seeds)
 import Url exposing (Url)
 
 
